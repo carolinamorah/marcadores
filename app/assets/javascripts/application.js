fced8b12513199e43ff41e1d9d35372d3391f102
Carolina mora <carolinamorahernandez@hotmail.com>
2021-09-19 06:30:24 -0300

    Update manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery/dist/jquery
+//= require jquery
 //= require chartkick
 //= require Chart.bundle
 //= require_tree .
